73e39ebcf33ad42c9fc4c3397988a3a9d461ae20
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2023-02-10 10:18:14 +0300

    old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,3 @@
+--list all bands with glam rock as the main stryle ranked by their longetivity
+
+select band_name, (ifnull(split, '2020') - formed) as lifespan from metal_bands where FIND_IN_SET('Glam rock', ifnull(style, "")) > 0 order by lifespan desc;
