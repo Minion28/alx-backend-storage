791cfe3ddcfb7b8bbef95e5095b79ee5ae430f32
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2023-02-10 10:07:16 +0300

    in and out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,12 @@
+--create table users
+--id int not null auto increment primary key
+--email string of 255 chars not null unique
+--name string of 255 chars
+--country enum of countries US, CO, and TN not null default =US
+
+create table if not exists users (
+    id int not null auto_increment primary key,
+    email varchar(255) not null unique,
+    name varchar(255),
+    country char(2) not null default 'US' check (country in ('US', 'CO', 'TN'))
+);
