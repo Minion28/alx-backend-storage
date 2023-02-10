2404efda305797ed137c9cfa08faeb743cb9b5d0
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2023-02-10 12:17:23 +0300

    safe divide

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+--create function SafeDiv that dividesthe first by the second number and returns 0 if the second number is 0
+
+DELIMITER $$
+create function SafeDiv (a int, b int)
+returns float deterministic begin
+declare result float default 0;
+
+if b != 0 then
+    set result = a / b;
+end if;
+return result;
+end $$
+DELIMITER ;
