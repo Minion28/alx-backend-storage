56522fd79ccc7a222be39394fd4a3c15bda4e7b1
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2023-02-10 13:50:53 +0300

    fix errors

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,9 +4,9 @@
 -- name string of 255 chars
 -- country enum of countries US, CO, and TN not null default =US
 
-create table if not exists users (
-    id int not null auto_increment primary key,
-    email varchar(255) not null unique,
-    name varchar(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL
 );
